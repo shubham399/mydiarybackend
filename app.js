30d4fc35213776e566ccf9a3e382675604253a57
Shubham <shubham.399@gmail.com>
2017-12-18 16:09:20 +0000

    Port Fix

--- app.js
+++ app.js
@@ -5,4 +5,4 @@ var app = express()
 app.get('/', function (req, res) {
   res.send('hello world')
 })
-app.listen(8080, () => console.log('Example app listening on port '+ 8080))
+app.listen(process.env.PORT, () => console.log('Example app listening on port '+ process.env.PORT))

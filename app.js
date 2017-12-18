15e2000f70429923bfebd8f5a579c78749d693de
Shubham <shubham.399@gmail.com>
2017-12-18 16:55:41 +0000

    Redis added

--- app.js
+++ app.js
@@ -1,16 +1,39 @@
 var express = require('express')
 var app = express()
 var bodyParser = require('body-parser');
+var redis = require("redis");
+const redisurl =process.env.REDIS_URL;
+var client = redis.createClient({url:redisurl});
 // Body Parser
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
   res.send('UP')
+});
+app.post('/add',function(req,res)
+{
+    if(req.headers["x-api-key"] === "somekey")
+    {
+    client.set("key",req.body.key);
+    res.send("SCCESS")
+    }
+    else
+    res.send({
+        "error":true,
+        "message":"Invalid Key"
+    })
 })
 app.get("/getall",function(req,res){
      if(req.headers["x-api-key"] === "somekey")
-    res.send({"error":false,"result":"Wow"});
+    {
+         client.get("key",function(err, reply){
+            if(reply !=null)
+             res.send(reply);
+             else
+             res.send("Error");
+         });
+    }
     else
     res.send({
         "error":true,

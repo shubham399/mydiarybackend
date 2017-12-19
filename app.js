b9e51ccad9683569586ca0e60644f376fc19d36c
Shubham <shubham.399@gmail.com>
2017-12-19 09:11:41 +0000

    Diary Routes moved in Routes Folder

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express')
 var app = express()
 var bodyParser = require('body-parser');
 var redis = require("redis");
+const diaryRouter = require("./routes/diary")
 const redisurl =process.env.REDIS_URL;
 var client = redis.createClient({url:redisurl});
 // Body Parser
@@ -11,33 +12,5 @@ app.use(bodyParser.json());
 app.get('/', function (req, res) {
   res.send('UP')
 });
-app.post('/add',function(req,res)
-{
-    if(req.headers["x-api-key"] === "somekey")
-    {
-    client.set("key",req.body.key);
-    res.send("SCCESS")
-    }
-    else
-    res.send({
-        "error":true,
-        "message":"Invalid Key"
-    })
-})
-app.get("/getall",function(req,res){
-     if(req.headers["x-api-key"] === "somekey")
-    {
-         client.get("key",function(err, reply){
-            if(reply !=null)
-             res.send(reply);
-             else
-             res.send("Error");
-         });
-    }
-    else
-    res.send({
-        "error":true,
-        "message":"Invalid Key"
-    })
-})
+app.use("/diary", diaryRouter);
 app.listen(process.env.PORT, () => console.log('Example app listening on port '+ process.env.PORT))
---
+++ routes/diary.js
@@ -0,0 +1,32 @@
+const router = require("express").Router();
+router.get("/",function(req,res){
+    res.send("UP");
+})
+router.post("/",function(req,res){
+    let output = addrecord(req.body);
+    res.send(output);
+})
+router.delete("/:id",function(req,res){
+    req.body.id = req.params.id;
+    let output = deleterecord(req.body);
+    res.send(output);
+})
+router.patch("/:id",function(req,res){
+    req.body.id = req.params.id;
+    let output = updaterecord(req.body);
+    res.send(output);
+})
+
+const addrecord = (value) =>{
+    return {"error":false,"result":"Need to Complete the Function"}
+}
+
+const deleterecord = (value) =>{
+    return {"error":false,"result":"Need to Complete the Function"}
+}
+
+const updaterecord = (value) =>{
+    return {"error":false,"result":"Need to Complete the Function"}
+}
+
+module.exports = router;

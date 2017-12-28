812209d153c16a368d4a08520394f81fd10194de
Shubham <shubham.399@gmail.com>
2017-12-28 12:38:04 +0000

    API key middleware added

--- config/config.js
+++ config/config.js
@@ -1,11 +1,11 @@
 module.exports = {
   development: {
     connection_url: process.env.DATABASE_URL,
-    api_key:"somekey"
+    api_key:"99bff15d-d4fe-44dd-a0a3-25f177535450"
   },
   test: {
      connection_url: process.env.DATABASE_URL,
-     api_key:"somekey"
+     api_key:"99bff15d-d4fe-44dd-a0a3-25f177535450"
   },
   production: {
    connection_url: process.env.HEROKU_POSTGRESQL_PUCE_URL,

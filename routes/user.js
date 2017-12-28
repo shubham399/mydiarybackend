a1879f3d78d3a765fa8924fc669b343411f7cd04
Shubham <shubham.399@gmail.com>
2017-12-28 10:47:22 +0000

    user register

--- routes/user.js
+++ routes/user.js
@@ -1,20 +1,35 @@
 const router = require("express").Router();
 const users = require('../models/users');
-const { check, validationResult } = require('express-validator/check');
-
+const helper = require("../utils/helper");
 router.get("/",function(req,res){
     res.send("UP");
 })
 router.post("/register",function(req,res){
     req.checkBody("email", "Enter a valid email address.").isEmail();
+    req.sanitizeBody("username");
+    req.sanitizeBody("password");
+    var errors = req.validationErrors();
+  if (errors) {
+    res.send(errors);
+    return;
+  } else {
+    register(req.body,(code,val)=>{
+        res.status(code);
+        res.send(val);
+    });
+  }
     
-    res.send(register(req.body));
 })
 router.post("/login",function(req,res){
     res.send(login(req))
 })
 
-const register =(state)=>{
+const register =(state,callback)=>{
+    state["userkey"]=helper.getuuid();
+    state.password = helper.gethash(state.password);
+    users.create(state).then(
+        callback(200,{"status":"SUCCESS","desc":"User Register Successfully"})
+    )
      
 }
 const login = (state) => {
---
+++ utils/helper.js
@@ -0,0 +1,5 @@
+const uuidv4 = require('uuid/v4');
+
+const getuuid =() =>{
+    return uuidv4();
+}

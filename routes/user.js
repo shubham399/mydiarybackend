627ac9e4efc3b17712b9bdcbbab89e8125e06d7c
Shubham <shubham.399@gmail.com>
2017-12-28 12:10:30 +0000

    Login

--- routes/user.js
+++ routes/user.js
@@ -5,39 +5,51 @@ router.get("/",function(req,res){
     res.send("UP");
 })
 router.post("/register",function(req,res){
-    req.checkBody("email", "Enter a valid email address.").isEmail();
-    req.sanitizeBody("username");
-    req.sanitizeBody("password");
+    req.checkBody("password", "Password must contain a number.").isLength({ min: 5 }).matches(/\d/);
+    req.checkBody("username", "Enter a valid Username").isLength({ min: 3 });
     var errors = req.validationErrors();
   if (errors) {
     res.send(errors);
     return;
   } else {
     register(req.body,(val)=>{
-        console.log("---CALLBACK ----");
         res.send(val);
     });
   }
     
 })
 router.post("/login",function(req,res){
-    res.send(login(req))
+    req.checkBody("password", "Password must contain a number.").isLength({ min: 5 }).matches(/\d/);
+    req.checkBody("username", "Enter a valid Username").isLength({ min: 3 });
+    var errors = req.validationErrors();
+  if (errors) {
+    res.send(errors);
+    return;
+  } else {
+    login(req.body,(val)=>{
+        res.send(val);
+    });
+  }
 })
 
 const register =(state,callback)=>{
     state["userkey"]=helper.getuuid();
     state.password = helper.gethash(state.password);
     models.User.create(state).then((val)=>{
-        console.log("---REGISTER SUCCESS----Reaching Here")
-        callback({"status":"SUCCESS","desc":"User Register Successfully"})}
+         callback({"status":"SUCCESS","desc":"User Register Successfully"})}
     ).catch((err)=>{
-        console.log("---REGISTER FAILURE----Reaching Here")
         callback(err);
     })
      
 }
-const login = (state) => {
-    
+const login = (state,callback) => {
+    state.password = helper.gethash(state.password);
+    models.User.findOne({ where: {username: state.username,password:state.password} }).then((val)=>{
+        
+         callback(val.dataValues)}
+    ).catch((err)=>{
+        callback(err);
+    })
 }
 
 module.exports = router;

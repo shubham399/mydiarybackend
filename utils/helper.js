af198a29abe9a82f9fc9418fe641fcc43ee8525d
Shubham <shubham.399@gmail.com>
2017-12-28 10:57:47 +0000

    get hash function added

--- utils/helper.js
+++ utils/helper.js
@@ -1,5 +1,9 @@
 const uuidv4 = require('uuid/v4');
-
+const crypto = require('crypto');
 const getuuid =() =>{
     return uuidv4();
+}
+const gethash=(message) =>{
+    const hash = crypto.createHash('sha256');
+    return hash.update(message).digest("base64");
 }

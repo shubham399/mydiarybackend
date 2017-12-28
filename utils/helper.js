78b599c6e1751b6d1062a8d4955561f93b3d3880
Shubham <shubham.399@gmail.com>
2017-12-28 11:33:20 +0000

    Some Fix

--- utils/helper.js
+++ utils/helper.js
@@ -7,3 +7,6 @@ const gethash=(message) =>{
     const hash = crypto.createHash('sha256');
     return hash.update(message).digest("base64");
 }
+
+exports.getuuid = getuuid;
+exports.gethash = gethash;

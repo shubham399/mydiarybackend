76e4d387b480995b684ea07e72cc56a282b694ab
Shubham <shubham.399@gmail.com>
2018-01-02 16:57:31 +0530

    cleanup

--- routes/diary.js
+++ routes/diary.js
@@ -109,7 +109,7 @@ const getone = (state,callback) =>{
         delete val["UserId"];
          callback({"error":false,"data":val});
      }).catch((err)=>{
-         callback({error:true,"status":"FAILURE","message":"Something Went Wrong","emsg":err});
+         callback({error:true,"status":"FAILURE","message":"Something Went Wrong"});
      })
 }
 module.exports = router;

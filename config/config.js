c6ab254e24c8d328e0a208cc51e2f9310156d9d1
Shubham <shubham.399@gmail.com>
2018-01-16 18:55:02 +0530

    Update config.js
    Updated production DB_URL

--- config/config.js
+++ config/config.js
@@ -8,7 +8,7 @@ module.exports = {
      api_key:"99bff15d-d4fe-44dd-a0a3-25f177535450"
   },
   production: {
-   connection_url: process.env.HEROKU_POSTGRESQL_PUCE_URL,
+   connection_url: process.env.DATABASE_URL,
    api_key:process.env.API_KEY
   }
 };

726ce1619267b05afd0d32531d568eedb5690501
Shubham <shubham.399@gmail.com>
2018-01-25 21:17:48 +0000

    DB model update

--- models/user.js
+++ models/user.js
@@ -9,7 +9,8 @@ module.exports = (sequelize, Sequelize) => {
     username:{type:Sequelize.STRING,unique:true},
     password:{type:Sequelize.STRING},
     email:{type:Sequelize.STRING,unique:true},
-    userkey:{type:Sequelize.STRING,unique:true}
+    userkey:{type:Sequelize.STRING,unique:true},
+    token:{type:Sequelize.STRING,defaultValue:null}
   });
 User.associate = function(models) {
     models.User.hasMany(models.Diary);

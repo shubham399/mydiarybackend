b1b048d22910ca47b98f096fb2385b5b808ffbec
Shubham <shubham.399@gmail.com>
2018-01-27 17:42:01 +0000

    diary

--- routes/diary.js
+++ routes/diary.js
@@ -1,19 +1,15 @@
 const router = require("express").Router();
-const models = require('../models');
-const crypto = require("../utils/crypto");
-const helper = require("../utils/helper");
-const env = process.env.NODE_ENV || 'development';
-const config = require("../config/config")[env];
+const diary = require("../services/diary")
 
 router.get("/",function(req,res){
-    getall(req.body,(val)=>{
+    diary.getall(req.body,(val)=>{
         res.send(val);
     })
 });
 
 router.get("/:id", function(req, res) {
   req.body["id"] = req.params.id;
-  getone(req.body, (val) => {
+  diary.getone(req.body, (val) => {
     res.send(val);
   })
 });
@@ -30,7 +26,7 @@ router.post("/", function(req, res) {
     res.send(errors);
     return;
   } else {
-    addrecord(req.body, (val) => {
+    diary.addrecord(req.body, (val) => {
       res.send(val);
     });
   }
@@ -38,146 +34,16 @@ router.post("/", function(req, res) {
 
 router.delete("/:id", function(req, res) {
   req.body["id"] = req.params.id;
-  deleterecord(req.body, (val) => {
+  diary.deleterecord(req.body, (val) => {
     res.send(val);
   });
 })
 
 router.patch("/:id", function(req, res) {
   req.body["id"] = req.params.id;
-  updaterecord(req.body, (val) => {
+  diary.updaterecord(req.body, (val) => {
     res.send(val);
   });
 })
-
-const addrecord = (state, callback) => {
-  state.note = crypto.encrypt(state.note,state.UserId)
-  models.Diary.create(state).then((val) => {
-    callback({
-      "error": false,
-      "data": {
-        "id": val.id,
-        "status": "SUCCESS",
-        "desc": "Record Created Successfully"
-      }
-    })
-  }).catch((err) => {
-    callback({
-      error: true,
-      "status": "FAILURE",
-      "message": "Something Went Wrong"
-    });
-  })
-}
-
-const deleterecord = (state, callback) => {
-  models.Diary.destroy({
-    where: {
-      id: state.id
-    }
-  }).then((val) => {
-    if (val)
-      callback({
-        "error": false,
-        "status": "SUCCESS",
-        "message": "Data Deleted"
-      })
-    else
-      callback({
-        "error": true,
-        "status": "FAILURE",
-        "message": "Data Not Found"
-      })
-  }).catch((err) => {
-    callback({
-      error: true,
-      "status": "FAILURE",
-      "message": "Something Went Wrong"
-    });
-  })
-}
-
-const updaterecord = (value, callback) => {
-  models.Diary.update({
-    title: value.title,
-    note: crypto.encrypt(value.note,value.UserId)
-  }, {
-    where: {
-      "id": value.id
-    }
-  }).then((val) => {
-
-    if (val[0])
-      callback({
-        "error": false,
-        "status": "SUCCESS",
-        "message": "Data Updated"
-      })
-    else
-      callback({
-        "error": true,
-        "status": "FAILURE",
-        "message": "Data Not Found"
-      })
-  }).catch((err) => {
-   callback({
-      error: true,
-      "status": "FAILURE",
-      "message": "Something Went Wrong"
-    });
-  })
-}
-
-const getall = (state, callback) => {
-  models.Diary.findAll({
-    where: {
-      UserId: state.UserId
-    }
-  }).then((val) => {
-    val = val.map(function(x) {
-      x = x.dataValues;
-      x.note = crypto.decrypt(x.note,x.userId);
-      x = helper.clean(x,["createdAt","updateAt","userId"])
-    })
-    callback({
-      "error": false,
-      "data": val
-    });
-  }).catch((err) => {
-    callback({
-      error: true,
-      "status": "FAILURE",
-      "message": "Something Went Wrong"
-    });
-  })
-}
-
-const getone = (state, callback) => {
-  models.Diary.findOne({
-    where: {
-      UserId: state.UserId,
-      id: state.id
-    }
-  }).then((val) => {
-    if (val.dataValues == [] || val.dataValues == null)
-      callback({
-        "error": false,
-        "data": val
-      });
-    val = val.dataValues;
-    val.note = crypto.decrypt(val.note,val.userId);
-    val = helper.clean(val,["createdAt","updateAt","userId"])
-    callback({
-      "error": false,
-      "data": val
-    });
-  }).catch((err) => {
-    callback({
-      error: true,
-      "status": "FAILURE",
-      "message": "Something Went Wrong"
-    });
-  })
-}
 
 module.exports = router;
---
+++ services/diary.js
@@ -0,0 +1,141 @@
+const models = require('../models');
+const crypto = require("../utils/crypto");
+const helper = require("../utils/helper");
+const env = process.env.NODE_ENV || 'development';
+const config = require("../config/config")[env];
+
+const addrecord = (state, callback) => {
+  state.note = crypto.encrypt(state.note,state.UserId)
+  models.Diary.create(state).then((val) => {
+    callback({
+      "error": false,
+      "data": {
+        "id": val.id,
+        "status": "SUCCESS",
+        "desc": "Record Created Successfully"
+      }
+    })
+  }).catch((err) => {
+    callback({
+      error: true,
+      "status": "FAILURE",
+      "message": "Something Went Wrong"
+    });
+  })
+}
+
+const deleterecord = (state, callback) => {
+  models.Diary.destroy({
+    where: {
+      id: state.id
+    }
+  }).then((val) => {
+    if (val)
+      callback({
+        "error": false,
+        "status": "SUCCESS",
+        "message": "Data Deleted"
+      })
+    else
+      callback({
+        "error": true,
+        "status": "FAILURE",
+        "message": "Data Not Found"
+      })
+  }).catch((err) => {
+    callback({
+      error: true,
+      "status": "FAILURE",
+      "message": "Something Went Wrong"
+    });
+  })
+}
+
+const updaterecord = (value, callback) => {
+  models.Diary.update({
+    title: value.title,
+    note: crypto.encrypt(value.note,value.UserId)
+  }, {
+    where: {
+      "id": value.id
+    }
+  }).then((val) => {
+
+    if (val[0])
+      callback({
+        "error": false,
+        "status": "SUCCESS",
+        "message": "Data Updated"
+      })
+    else
+      callback({
+        "error": true,
+        "status": "FAILURE",
+        "message": "Data Not Found"
+      })
+  }).catch((err) => {
+   callback({
+      error: true,
+      "status": "FAILURE",
+      "message": "Something Went Wrong"
+    });
+  })
+}
+
+const getall = (state, callback) => {
+  models.Diary.findAll({
+    where: {
+      UserId: state.UserId
+    }
+  }).then((val) => {
+    val = val.map(function(x) {
+      x = x.dataValues;
+      x.note = crypto.decrypt(x.note,x.userId);
+      x = helper.clean(x,["createdAt","updateAt","userId"])
+    })
+    callback({
+      "error": false,
+      "data": val
+    });
+  }).catch((err) => {
+    callback({
+      error: true,
+      "status": "FAILURE",
+      "message": "Something Went Wrong"
+    });
+  })
+}
+
+const getone = (state, callback) => {
+  models.Diary.findOne({
+    where: {
+      UserId: state.UserId,
+      id: state.id
+    }
+  }).then((val) => {
+    if (val.dataValues == [] || val.dataValues == null)
+      callback({
+        "error": false,
+        "data": val
+      });
+    val = val.dataValues;
+    val.note = crypto.decrypt(val.note,val.userId);
+    val = helper.clean(val,["createdAt","updateAt","userId"])
+    callback({
+      "error": false,
+      "data": val
+    });
+  }).catch((err) => {
+    callback({
+      error: true,
+      "status": "FAILURE",
+      "message": "Something Went Wrong"
+    });
+  })
+}
+
+exports.addrecord =addrecord;
+exports.deleterecord =deleterecord;
+exports.updaterecord = updaterecord;
+exports.getall =getall;
+exports.getone =getone;

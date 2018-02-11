43222bf0dcddfbdead8c7051829475b454ac16c7
Shubham <shubham.399@gmail.com>
2018-02-11 17:47:23 +0000

    fixes

--- services/redis.js
+++ services/redis.js
@@ -4,4 +4,12 @@ var redis = require("redis");
 console.log(redisURL)
 const client = redis.createClient(redisURL);
 
+const set  = function(key,value){
+    return client.set(key,value, redis.print);
+}
+const get = function(key,cb){
+    client.get(key,cb)
+}
 exports.redis = client;
+exports.set = set;
+exports.get = get;

c7363646e646d51773fe71bbde82cad947dc1a04
Shubham <shubham.399@gmail.com>
2018-02-11 17:41:26 +0000

    redis url update

--- services/redis.js
+++ services/redis.js
@@ -1,6 +1,7 @@
 const env = process.env.NODE_ENV || "development"
-const redisURL = require("../config/config")[env];
+const redisURL = require("../config/config")[env].redisURL;
 var redis = require("redis");
+console.log(redisURL)
 const client = redis.createClient(redisURL);
 
 exports.redis = client;
